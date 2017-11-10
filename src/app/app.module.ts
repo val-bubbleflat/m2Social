00cc1a29613494b27a54c8f5cd1faecd3f2257c5
Val <valentin@bubbleflat.com>
2017-11-10 11:58:49 +0100

    lazy loading de la liste des utilisateurs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
     {path: '', redirectTo: '/posts', pathMatch: 'full'},
+    {path: 'users',  loadChildren: 'app/user/user.module#UserModule'}
 ];
 
 @NgModule({
